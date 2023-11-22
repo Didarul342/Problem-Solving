f3ccf222df54c6f33cd23f7a2df3813f3a270907
Didarul Islam <149935316+Didarul342@users.noreply.github.com>
2023-11-22 23:00:23 +0600

    Add files via upload

--- C/Others/Add - Recursion.c
+++ C/Others/Add - Recursion.c
@@ -1,19 +1,15 @@
 #include <assert.h>
 #include <stdio.h>
 
-//
-// A program to add two positive numbers
-//
 int add_recursive(int x, int y) {
   assert(y >= 0); // precondition check.
   if (y == 0) {
-    // The base case.
+ 
     return x;
   } else {
-    // Pass most of the work to your copy.
+
     int result = add_recursive(x, y - 1);
 
-    // Do your work.
     return result + 1;
   }
 }
